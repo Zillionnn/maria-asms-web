17ec4c2672dc3bf929622026e3191939c467735a
zillionnn <skyestzhang@163.com>
2019-07-19 21:45:14 +0800

    fix bug

--- src/api/file.js
+++ src/api/file.js
@@ -1,11 +1,9 @@
-// import {http} from './ind/ex'
+// import {http} from './index'
 import conf from '@/conf/index'
-
-const url = conf.URL
 
 export const file = {
   exportExcel (planId) {
-    window.open(`http://${url}/api/v1/exportXlsx/` + planId)
+    window.open(`${conf.URL}/api/v1/exportXlsx/` + planId)
 
     // return http.get(`/api/v1/exportXlsx`)
   }
